ce77a40224ebbd143b1182200aaca38c6a3def2f
Andrey <anarki3@yandex.ru>
2018-09-05 20:48:25 +0300

    конец 15го урока

--- components/chat/chat.js
+++ components/chat/chat.js
@@ -1,26 +1,44 @@
 (function(){
     'use strict';
 
-    class Chat {
-        constructor({el}) {
-            this.el = el;
+    /**
+     * @typedef {Object} ChatMessage
+     * @property {string} username
+     * @property {string} message
+     */
 
+    class Chat {
+        constructor({el, data = {messages: []}}) {
+            this.el = el;
+            this.data = data; // массив сообщений
         }
 
         render () {
+
+            let messagesHTML = this.data.messages.map(mData => {
+               return `<div class="chat__message">
+                        <span class="chat__author">${mData.username}</span>
+                        ${mData.message}
+                    </div>`;
+            }).join('<br>');
+
             this.el.innerHTML = `
                 <div class="chat">
                     <div class="chat__message chat__message_my">
                         <span class="chat__author">Tim</span>
                         Всем привет!
-                    </div>
-                    <div class="chat__message">
-                        <span class="chat__author">Nikolay</span>
-                        Привет, верстальщики!
-                    </div>
-                    
+                    </div>                                       
                 </div>
             `;
+        }
+
+        /**
+         * Add message into chat store. Without rerender
+         * @param {ChatMessage} данные сообщения
+         */
+
+        addMessage (message) { // Кто-то должен вызвать этот метод и добавить сообщение в массив this.data в конструктор
+            this.data.messages.push(message);
         }
     }
 
